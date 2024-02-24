511d76415a9ab021351892c2aab2e4ecd8cebb03
Eshan Singh Parihar <eshaan.singh.parihar@gmail.com>
2024-02-25 04:22:01 +0530

    hamburger added, footer made responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header() {
+
+const [isOpen, setIsOpen] = useState(false);
   return (
     <header className='flex items-center flex-row justify-between p-2'>
         <div className='flex flex-row'>
@@ -20,36 +22,80 @@ export default function Header() {
         </div>
         <div>
         <nav className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Home</a>
-            <div class="relative group">
-                <a href="#" class="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Hostels</a>
-                <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
-                    <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Munnar</a>
-                    <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Vagamon</a>
-                </div>
+        <div className="max-md:hidden flex items-center">
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Home</a>
+        <div class="relative group">
+            <a href="#" class="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Hostels</a>
+            <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Munnar</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Vagamon</a>
             </div>
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">About Us</a>
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">FAQ's</a>
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Careers</a>
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Contact</a>
-           <div class="relative group">
-                <a href="#" class="font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">More</a>
-                <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
-                    <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Members</a>
-                    <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Forum</a>
-                    <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Group</a>
-                </div>
-            </div>
-            <a href="#" 
-            className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Log In</a>
         </div>
-        </nav>   
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">About Us</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">FAQ's</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Careers</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Contact</a>
+        <div class="relative group">
+            <a href="#" class="font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">More</a>
+            <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Members</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Forum</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Group</a>
+            </div>
+        </div>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Log In</a>
+        </div>
+        <div className="lg:hidden flex items-center">
+        <button aria-label="Toggle Menu" onClick={() => setIsOpen(!isOpen)}>
+          <svg className="w-6 h-6 fill-current text-gray-500" viewBox="0 0 24 24">
+            {isOpen ? (
+              <path d="M6 18L18 6M6 6l12 12" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+            ) : (
+              <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+            )}
+          </svg>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto bg-white" style={{ height: '100vh' }}>
+          {/* Sidebar Menu Content */}
+          <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Home</a>
+        <div class="relative group">
+            <a href="#" class="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Hostels</a>
+            <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Munnar</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Vagamon</a>
+            </div>
+        </div>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">About Us</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">FAQ's</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Careers</a>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Contact</a>
+        <div class="relative group">
+            <a href="#" class="font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">More</a>
+            <div class="absolute hidden group-hover:block mt-2 py-2 px-4 rounded-lg">
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Members</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Forum</a>
+                <a href="#" class="block uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Group</a>
+            </div>
+        </div>
+        <a href="#" 
+        className="uppercase font-light text-xs hover:text-white text-black text-lg font-bold mr-4 hover:bg-gray-700 py-2 px-4 rounded">Log In</a>
+          {/*Ends here*/}
+        </div>
+      )}
+    </nav>
         </div>
     </header>
   )
